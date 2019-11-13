3bb56afaacaa338849e10d38c11820f2531e767c
Tiago Umemura <umemuratiago@gmail.com>
2019-11-12 22:32:47 -0200

    Add redux persit to persis auth on localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import auth from './ducks/auth/reducers';
 import rootSaga from './rootSaga'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'] //Persists auth when browser refresh
+}
+
 const sagaMiddleware = createSagaMiddleware()
-const reducers = combineReducers({
+const rootReducers = combineReducers({
   auth,
 });
 
-export const Store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const persistedReducer = persistReducer(persistConfig, rootReducers)
+
+export const Store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+export const Persistor = persistStore(Store);
 
 sagaMiddleware.run(rootSaga)
