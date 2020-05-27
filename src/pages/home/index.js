b037f579fc03dafa152d4ca7f063fc6d5a45bd9c
Tiago Umemura <umemuratiago@gmail.com>
2020-05-27 20:01:26 -0300

    Add route wrapper with header and remove unnecessary filesand add global styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import { Container } from './styles';
-import Header from '../../components/header';
 
 function Home() {
 
   return (
     <Container>
-      <Header />
     </Container>
   );
 }
