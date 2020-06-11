992489a4cedbeed29ead8f08461851df5d6acda4
Tiago Umemura <umemuratiago@gmail.com>
2020-06-11 17:57:33 -0300

    call socket action that pass in middleware

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
+import { useDispatch } from 'react-redux'
 
 import { Container } from './styles';
 import Dropdown from 'components/dropdown/dropdown'
+import Button from 'components/button'
+import {socketConnect, sendMessage} from 'store/ducks/socket/actions'
 
 function Home() {
   const [value, setValue] = useState('')
+  const dispatch = useDispatch()
 
   const DropdownValues = [
     { fields: ['Test'], value: 'test' },
@@ -23,6 +27,20 @@ function Home() {
         handleSelect={onSelect}
         width='125px'
         height='50px'
+      />
+      <Button
+        bgColor='black'
+        width='125px'
+        height='50px'
+        text='Socket Connect'
+        onClickHandler={() => dispatch(socketConnect())}
+      />
+      <Button
+        bgColor='black'
+        width='125px'
+        height='50px'
+        text='Socket Send Message'
+        onClickHandler={() => dispatch(sendMessage('message text'))}
       />
     </Container>
   );
