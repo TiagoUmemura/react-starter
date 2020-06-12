6a3b0e3dc815eab0b8bfd2a9480bbfe345590eab
Tiago Umemura <umemuratiago@gmail.com>
2020-06-12 20:17:44 -0300

    Add toast notification example

--- src/store/ducks/auth/saga.js
+++ src/store/ducks/auth/saga.js
@@ -1,4 +1,6 @@
 import { put, takeLatest, all } from 'redux-saga/effects'
+
+import { toast } from 'react-toastify';
 import history from '../../../history/history'
 
 export function * login(action) {
@@ -6,6 +8,7 @@ export function * login(action) {
     //    const user = yield call(Api.fetchUser, action.payload.userId);
     //    yield put({type: "USER_FETCH_SUCCEEDED", user: user});
         console.log(action.payload.username)
+        toast.success('login success')
         history.push('/home')
     } catch (e) {
        yield put({type: "USER_FETCH_FAILED", message: e.message});
@@ -15,6 +18,7 @@ export function * login(action) {
 export function * logout() {
    try {
          history.push('/')
+         toast.info('logout success')
    } catch (e) {
       yield put({type: "USER_FETCH_FAILED", message: e.message});
    }
