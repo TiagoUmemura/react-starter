fc8789ddfdcb3c5c8964066b556f8268ddbb7f50
Tiago Umemura <umemuratiago@gmail.com>
2020-06-20 12:32:10 -0300

    add axios interceptior and api call example

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,33 @@
-import axios from "axios";
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+import { Store } from 'store/index'
+import { logout } from 'store/ducks/auth/actions'
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:3333"
 });
+
+api.interceptors.request.use(async config => {
+  const auth = JSON.parse(localStorage.getItem('persist:authKey'))
+  const token = JSON.parse(auth.auth)
+  if (auth) {
+    config.headers.Authorization = `Bearer ${token.token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(response => { return response }, error => {
+  if (error.response.data.message) {
+    toast.error(JSON.stringify(error.response.data.message))
+  } else {
+    toast.error(`${error.response.statusText} - ${error.response.config.url}`)
+  }
+
+  if (error.response.status === 401) {
+    Store.dispatch(logout())
+  }
+  throw error
+})
 
 export default api;
--- src/store/ducks/auth/saga.js
+++ src/store/ducks/auth/saga.js
@@ -2,14 +2,18 @@ import { put, takeLatest, all } from 'redux-saga/effects'
 
 import { toast } from 'react-toastify';
 import history from '../../../history/history'
+// import api from 'services/api'
 
 export function * login(action) {
     try {
-    //    const user = yield call(Api.fetchUser, action.payload.userId);
-    //    yield put({type: "USER_FETCH_SUCCEEDED", user: user});
-        console.log(action.payload.username)
-        toast.success('login success')
-        history.push('/home')
+      // const response = yield call(
+      //    api.post,
+      //    `/v1/companies/${payload.companyId}/numbers`,
+      //    payload.formNumber
+      //  )
+      console.log(action.payload.username)
+      toast.success('login success')
+      history.push('/home')
     } catch (e) {
        yield put({type: "USER_FETCH_FAILED", message: e.message});
     }
