e99a74bd5b6bcb26ba6bf31de8356e74da9e1ff2
Tiago Umemura <umemuratiago@gmail.com>
2020-06-20 19:28:37 -0300

    config eslint airbnb

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,40 @@
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import axios from "axios";
+import { toast } from "react-toastify";
 
-import { Store } from 'store/index'
-import { logout } from 'store/ducks/auth/actions'
+import { Store } from "store/index";
+import { logout } from "store/ducks/auth/actions";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333"
+  baseURL: "http://127.0.0.1:3333",
 });
 
-api.interceptors.request.use(async config => {
-  const auth = JSON.parse(localStorage.getItem('persist:authKey'))
-  const token = JSON.parse(auth.auth)
+api.interceptors.request.use(async (config) => {
+  const auth = JSON.parse(localStorage.getItem("persist:authKey"));
+  const token = JSON.parse(auth.auth);
   if (auth) {
-    config.headers.Authorization = `Bearer ${token.token}`
+    config.headers.Authorization = `Bearer ${token.token}`;
   }
-  return config
-})
+  return config;
+});
 
-api.interceptors.response.use(response => { return response }, error => {
-  if (error.response.data.message) {
-    toast.error(JSON.stringify(error.response.data.message))
-  } else {
-    toast.error(`${error.response.statusText} - ${error.response.config.url}`)
-  }
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.data.message) {
+      toast.error(JSON.stringify(error.response.data.message));
+    } else {
+      toast.error(
+        `${error.response.statusText} - ${error.response.config.url}`
+      );
+    }
 
-  if (error.response.status === 401) {
-    Store.dispatch(logout())
+    if (error.response.status === 401) {
+      Store.dispatch(logout());
+    }
+    throw error;
   }
-  throw error
-})
+);
 
 export default api;
