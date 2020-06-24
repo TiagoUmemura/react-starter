30dbe8d82db9791174a2125f83dd7367958ebc75
Tiago Umemura <umemuratiago@gmail.com>
2020-06-23 21:22:55 -0300

    Add theme provider and use theme.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import { Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import { Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "styled-components";
+import { projectTheme } from "theme";
 
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { Store, Persistor } from './store'
-import GlobalStyle from './global-style'
-import RouteWrapper from './routes/route'
+import { Store, Persistor } from "./store";
+import GlobalStyle from "./global-style";
+import RouteWrapper from "./routes/route";
 
-import Login from './pages/login'
-import Home from './pages/home'
-import history from './history/history'
+import Login from "./pages/login";
+import Home from "./pages/home";
+import history from "./history/history";
 
 function App() {
   return (
     <Provider store={Store}>
-        <PersistGate loading={null} persistor={Persistor}>
+      <PersistGate loading={null} persistor={Persistor}>
+        <ThemeProvider theme={projectTheme}>
           <Router history={history}>
             <RouteWrapper path="/" exact={true} component={Login} />
             <RouteWrapper path="/home" exact={true} component={Home} />
             <GlobalStyle />
-            <ToastContainer autoClose={3000}/>
+            <ToastContainer autoClose={3000} />
           </Router>
-        </PersistGate>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
